414da78024f92e1d7fda472143975ac930d26e28
tanyayalovega <tanya01022002@gmail.com>
2022-05-24 20:33:17 +0300

    right structure

---
+++ HW/task 3/Vector/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Vector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrr = { 1, 2, 3, 3, 3, 3, 2, 2, 0, 0, 0, 0, 0 };
+            Vector arr = new Vector(arrr);
+            //arr.InitShuffle();
+            Console.WriteLine(arr);
+            Console.WriteLine(arr.Subsequence());
+            Console.WriteLine(arr);
+
+            //Console.WriteLine( arr.IsPalindrom());
+            /*try
+            {
+                arr[0] = 999;
+                Console.WriteLine(arr[21]);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }*/
+
+
+            /*            Pair[] pairs = arr.CalculateFreq();
+
+                        for (int i = 0; i < pairs.Length; i++)
+                        {
+                            Console.Write(pairs[i] + "\n"); 
+                        }
+                        Console.WriteLine();*/
+
+            //Console.WriteLine(pairs);
+            //arr.RandomInitialization();
+            //Console.WriteLine(arr);
+
+
+        }
+    }
+}
