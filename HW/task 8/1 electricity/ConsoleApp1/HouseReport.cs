4130a51f8148a87693ba0a14c93a065ccb062462
tanyayalovega <tanya01022002@gmail.com>
2022-06-21 12:24:07 +0300

    task 8_1

---
+++ HW/task 8/1 electricity/ConsoleApp1/HouseReport.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    enum Quarters
+    {
+        First = 1,
+        Second,
+        Third,
+        Fourth
+    }
+    internal class HouseReport:IReader
+    {
+        private Quarters quarterNumber;
+        HashSet<ApatrmentReport> quarterReports;
+        public HouseReport()
+        {
+            quarterReports = new();
+        }
+        public HouseReport(IEnumerable<ApatrmentReport> _qr, Quarters q_numb)
+        {
+            quarterReports = new();
+            foreach (ApatrmentReport q in _qr)
+                quarterReports.Add(q);
+        }
+        public ApatrmentReport this[int i]
+        {
+            get
+            {
+                return quarterReports.First(item => item.Appartment == i);
+            }
+        }
+        public void ReadFromStreamReader(StreamReader reader)
+        {
+            if (reader.EndOfStream)
+                throw new Exception("File is empty");
+            quarterNumber = (Quarters)Int32.Parse(reader.ReadLine());
+            int numberOfFlats = Int32.Parse(reader.ReadLine());
+            for (int i = 0; i < numberOfFlats; i++)
+            {
+                ApatrmentReport qr = new();
+                qr.ReadFromStreamReader(reader);
+                quarterReports.Add(qr);
+            }
+        }
+        public override string ToString()
+        {
+            string str =  ((int)quarterNumber).ToString() + "\n";
+            foreach (var el in quarterReports)
+                str += el.ToString() + "\n";
+            return str;
+        }
+        public void PrintToFileSrteam(StreamWriter writer)
+        {
+            writer.Write(this.ToString());
+        }
+
+        public ApatrmentReport MaxElectricityArrears(double uah_kwh)
+        {
+            /*int max = quarterReports.Max(x => x.QuarterUsing());
+            return quarterReports.First(x => x.QuarterUsing() == max);*/
+            
+            ApatrmentReport max = null;
+            int quarterKwh = 0;//використовуємо змінні для збереженя кіловат, щоб не викливати зайвий раз метод QuarterUsing()
+            int maxQuarterKwh = 0;
+            foreach (var item in quarterReports)
+            {
+                quarterKwh = item.QuarterUsing();
+                if (quarterKwh > maxQuarterKwh)
+                {
+                    max = item;
+                    maxQuarterKwh = quarterKwh;
+                }
+            }
+            return max;
+        }
+        public List<ApatrmentReport> ZeroUsing()
+        {
+            return quarterReports.Where(item => item.QuarterUsing() == 0).ToList();
+        }
+        public Dictionary<ApatrmentReport, double> QuarterPayments(double uah_kwh)
+        {
+            Dictionary<ApatrmentReport, double> result = new ();
+            foreach (var item in quarterReports)
+            {
+                result[item] = item.QuarterPayment(uah_kwh);
+            }
+            return result;
+        }
+
+        public Dictionary<ApatrmentReport, int> DaysFromLastReading()
+        {
+            Dictionary<ApatrmentReport, int> result = new ();
+            foreach (var item in quarterReports)
+            {
+                result[item] = (DateTime.Now - item.LastReading).Days;
+            }
+            return result;
+        }
+
+        public static HouseReport operator+(HouseReport first, HouseReport second)
+        {
+            if(first.quarterNumber != second.quarterNumber)
+                return null;
+            //HashSet<ApatrmentReport> firstSet = new(first.quarterReports);
+            //HashSet<ApatrmentReport> secondSet = new(second.quarterReports);
+            return new HouseReport(first.quarterReports.Union(second.quarterReports).ToList(), first.quarterNumber);
+            /*
+            HouseReport result = new HouseReport();
+            return new HouseReport( first.quarterReports.Concat(second.quarterReports).ToList(), first.quarterNumber);
+        */
+        }
+        public static HouseReport operator -(HouseReport first, HouseReport second)
+        {
+            if (first.quarterNumber != second.quarterNumber)
+                return null;
+            return new HouseReport(first.quarterReports.Except(second.quarterReports).ToList(), first.quarterNumber);
+        }
+    }
+}
---
+++ HW/task 8/1 electricity/ConsoleApp1/Month.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Month
+    {
+        private DateTime date;
+        private int meterReading;
+        public int MeterReading { get { return meterReading; }}
+        public Month()
+        {
+            date = new DateTime();
+        }
+        public Month(DateTime _date, int _meterReading)
+        {
+            date = _date;
+            meterReading = _meterReading;
+        }
+
+        public Month(string _date, string _number)
+        {
+            string[] arr = _date.Split('.');
+            date = new DateTime(2000 + int.Parse(arr[2]), int.Parse(arr[1]), int.Parse(arr[0]));
+            meterReading = int.Parse(_number);
+        }
+        public DateTime Date { get { return date; } }
+        public override string ToString()
+        {
+            return date.ToString("   dd MMMM yy - ") + String.Format("{0,-10}", meterReading);
+        }
+        public void PrintToFileSrteam(StreamWriter writer)
+        {
+            writer.WriteLine(this.ToString());
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Month month &&
+                   date == month.date &&
+                   meterReading == month.meterReading;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(date, meterReading);
+        }
+    }
+}
