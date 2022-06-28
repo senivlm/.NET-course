aec58cbfb3309d46ffb976790a6b4a62484a6885
senivlm <91743906+senivlm@users.noreply.github.com>
2022-06-28 10:06:56 +0300

    Update Program.cs

--- HW/task 9/Menu/Program.cs
+++ HW/task 9/Menu/Program.cs
@@ -12,6 +12,7 @@ namespace Menu
             PriceKurant prices = WorkWithFile.ReadPriceKurant(new StreamReader(@"D:\.NET course\Menu\Menu\Prices.txt"));
             Console.WriteLine(prices);
             double total = 0;
+            // тут треба перевірку, чи порахований результат
             MenuService.TryGetMenuTotalSum(menu, prices, out total);
             Console.WriteLine(total);
         }
