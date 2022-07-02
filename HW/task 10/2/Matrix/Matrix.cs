23c39e3737629a4b5e04732922cbaa474c209691
tanyayalovega <tanya01022002@gmail.com>
2022-07-02 11:56:26 +0300

    10_2

---
+++ HW/task 10/2/Matrix/Matrix.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix
+{
+    public class Matrix: IEnumerator
+    {
+        private int n;
+        private int m;
+        private int[,] arr;
+        private int positionI;
+        private int positionJ;
+        bool moveDown;
+        public Matrix()
+        {
+            n = 1;
+            m = 1;
+            arr = new int[n, m];
+            positionI = -1;
+            positionJ = 0;
+            moveDown = true;
+        }
+        public Matrix(int n_, int m_)
+        {
+            n = n_;
+            m = m_;
+            arr = new int[n, m];
+            int num = 0;
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < m; j++)
+                    arr[i, j] = num++;
+            positionI = -1;
+            positionJ = 0;
+            moveDown = true;
+        }
+        public object Current
+        {
+            get
+            {
+                try
+                {
+                    return arr[positionI, positionJ];
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    throw new InvalidOperationException();
+                }
+            }
+        }
+        public bool MoveNext()
+        {
+            
+            if (positionJ >= m)
+            {
+                return false;
+            }
+            if(moveDown)
+                ++positionI;
+            else
+                --positionI;
+
+            if (positionI >= n)
+            {
+                moveDown = false;
+                positionI = n - 1;
+                return ++positionJ < m;
+                
+            }
+            else if ( positionI < 0)
+            {
+                moveDown = true;
+                positionI = 0;
+                return ++positionJ < m;
+            }
+            else
+            {
+                return true;
+            }
+            
+        }
+        public void Reset()
+        {
+            positionI = -1;
+            positionJ = 0;
+            moveDown = true;
+        }
+        public IEnumerator GetEnumerator()
+        {
+            return (IEnumerator)this;
+        }
+
+        public void VerticalSnake()
+        {
+            int num = 1;
+            for (int j = 0; j < m;)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    arr[i, j] = num;
+                    num ++;
+                }
+                j++;
+                if (j == m)
+                    break;
+                for (int i = n - 1; i >= 0; i--)
+                {
+                    arr[i, j] = num;
+                    num++;
+                }
+                j++;
+            }
+        }
+        public void MainDiag(bool ToTheRight = false)
+        {
+            int num = 1;
+            int i = 0, j = 0;
+            if (ToTheRight)
+            {
+                arr[0, 0] = num;
+                num++;
+                j = 1;
+            }
+            for (; (j < m) || (i < n);)
+            {
+                for (; (i < n) && (j >= 0); ++i, --j)//вниз
+                {
+                    arr[i, j] = num;
+                    num++;
+                }
+                if (i >= n)
+                { --i; j += 2; }
+                else if (j < 0)
+                { ++j; }
+
+                for (; (i >= 0) && (j < m); --i, ++j)//вгору
+                {
+                    arr[i, j] = num;
+                    num++;
+                }
+                if (j >= m)
+                { --j; i += 2; }
+                else if (i < 0)
+                { ++i; }
+            }
+        }
+        public void MainDiagSquare()
+        {
+            if (n!=m )
+            {
+                Console.WriteLine("N != M - matrix is not square");
+                return;
+            }
+            int num = 1;
+            for (int i = 0, j = 0; (j < n) || (i < n);)
+            {
+                for (; (i < n) && (j >= 0); ++i, --j)//вниз
+                {
+                   
+                }
+            }
+        }
+        public void Spiral()
+        {
+            int num = 1;
+            float p = ((float)Math.Min(n, m)) / 2;//визначаємо, скільки буде кіл
+
+            int i = n, j = -1;
+            for (int step = 0; step < p; ++step)
+            {
+                for (++j, --i; (i >= step) && (arr[i, j] == 0); --i)//вгору
+                {
+                    arr[i, j] = num;
+                    num++;
+                }
+                for (++i, ++j; (j <= m - 1 - step) && (arr[i, j] == 0); ++j)//вправо
+                {
+                    arr[i, j] = num;
+                    num++;
+                }
+                for (--j, ++i; (i <= n - 1 - step) && (arr[i, j] == 0); ++i)//вниз
+                {
+                    arr[i, j] = num;
+                    num++;
+
+                }
+                for (--i, --j; (j >= 1 + step) && (arr[i, j] == 0); --j)//вліво
+                {
+                    arr[i, j] = num;
+                    num++;
+                }
+            }
+
+            /*for (int step = 0; step < p; ++step)
+            {
+                for (; (i > step) && (arr[i, j] == 0); --i)//вгору
+                {
+                    arr[i, j] = num;
+                    num++;
+                }
+                for (; (j < m - 1 - step) && (arr[i, j] == 0); ++j)//вправо
+                {
+                    arr[i, j] = num;
+                    num++;
+                }
+                for (; (i < n - 1 - step) && (arr[i, j] == 0); ++i)//вниз
+                {
+                    arr[i, j] = num;
+                    num++;
+
+                }
+                for (; (j > 1 + step) && (arr[i, j] == 0); --j)//вліво
+                {
+                    arr[i, j] = num;
+                    num++;
+                }
+            }*/
+
+            //if (arr[i, j] == 0)  
+            //arr[i, j] = num;
+
+        }
+
+        public void test()
+        {
+            
+            int middle = n / 2;
+            int a = middle;
+            for (int i = 0; i < middle; i++, a--)
+            {
+                for (int j = 0; j < a; j++)
+                {
+                    arr[i, j] = 1;
+                }
+            }
+            a = middle;
+            for (int i = 0; i < middle; i++, a++)
+            {
+                for (int j = a + 1; j < n; j++)
+                {
+                    arr[i, j] = 2;
+                }
+            }
+            a = 0;
+            for (int i = middle; i < n; i++, a++)
+            {
+                for (int j = 0; j < a; j++)
+                {
+                    arr[i, j] = 3;
+                }
+
+            }
+            a = 0;
+            for (int i = middle + 1; i < n; i++, a++)
+            {
+                for (int j = n - 1 - a; j < n; j++)
+                {
+                    arr[i, j] = 4;
+                }
+
+            }
+        }
+        public void Print()
+        {
+            for (int i = 0; i < n; ++i)
+            {
+                for (int j = 0; j < m; ++j)
+                {
+                    Console.Write(arr[i, j] + "\t");
+                }
+                Console.Write("\n");
+            }
+        }
+        
+
+       
+    }
+}
---
+++ HW/task 10/2/Matrix/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Matrix matr = new Matrix(3, 6);
+            //matr.MainDiag(true);
+            matr.Print();
+            foreach (var item in matr)
+            {
+                Console.WriteLine(item);
+            }
+           
+        }
+    }
+    
+    
+}
